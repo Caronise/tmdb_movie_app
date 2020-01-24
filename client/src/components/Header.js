61212f0bf83e35c267dba1bb4c854bcc68b5efd6
Caronise <lucas.betancourt@hotmail.com>
2020-01-23 21:08:06 -0500

    Changed from using a navbar to a container with rows and columns.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {
-  Navbar,
-  Image,
+  Container,
+  Row,
+  Col
 } from 'react-bootstrap';
 
 export default function Header() {
 
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>
-        <img
-          alt=""
-          src="../images/PrimaryLogo_Green.png"
-          width="50"
-          height="50"
-          className="d-inline-block align-top"
-        />{' '}
-        React Bootstrap
-      </Navbar.Brand>
-    </Navbar>
+    <Container>
+      <Row>
+        <Col className="logo-area">1 of 2</Col>
+        <Col className="search-area">2 of 2</Col>
+      </Row>
+    </Container>
+
   );
 }
 
